fd8e04d1cb66fe56b079cce63fec7b9c2d853cd0
Morgan Parry <morgan.parry@fortiumtech.com>
2020-07-14 10:30:36 +0100

    init commit

---
+++ precache-manifest.e871e813088a786fd2571cfdefb8da82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530539d2f9976edd9d4602f8c7fd1eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec472a160bc601151fcd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c777de4544085fa2194",
+    "url": "/static/js/2.d6f00988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6f00988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec472a160bc601151fcd",
+    "url": "/static/js/main.c5c94d9c.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  }
+]);
